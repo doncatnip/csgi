0cb08dbbae669af2a606aa28aa67a8c0d4b8622f
doncatnip <don.t@pan1.cc>
2011-12-12 01:26:27 +0000

    really add werkzeug app

---
+++ examples/crude_demo/testresource/wsgi/__init__.py
@@ -0,0 +1,10 @@
+class SimpleApp:
+
+    def __call__( self, environ, start_response ):
+        body = '<html><head><title>wsgi app</title></head><body><h1>hello wsgi</h1></body></html>'
+        response_headers = [
+            ("Content-type", "text/html"),
+            ]
+    
+        start_response("200 OK", response_headers)
+        return [body,]
