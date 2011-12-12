ad935cd8879c4353c73ca615bca674292e684969
doncatnip <don.t@pan1.cc>
2011-12-12 02:00:38 +0000

    rm leftover file

--- examples/crude_demo/testresource/wsgi.py
+++
@@ -1,7 +0,0 @@
-
-class WerkzeugApp:
-    from werkzeug.wrappers import Request, Response
-    
-    @Request.application
-    def __call__(self, request):
-        return self.Response('Hello World!')
